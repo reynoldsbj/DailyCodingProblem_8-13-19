b0389ef1aa16e23a7334db9f170c37296077781e
Brian <brianjreynolds@hotmail.com>
2019-08-14 23:58:51 -0400

    First commit for problem 8-13-19

---
+++ main.js
@@ -0,0 +1,11 @@
+/**
+ * Description: Given an array of integers, find the first missing positibe integer in linear time
+ * and constant space. In other words, find the lowest positive integer that does not exist in the
+ * array. The array an contain duplicates and negative numbers as well. 
+ * 
+ * Examples:
+ *  1: For the input [3, 4, -1, 1], return 2. 
+ *  2: For the input [1, 2, 0], return 3
+ * 
+ * Note: You can modify the input array in place.
+ */
---
+++ test/main.Spec.js
@@ -0,0 +1,11 @@
+describe('SolveProblem([3, 4, -1])', function() {
+    it('should return 2', function() {
+        expect(SolveProblem([3, 4, -1])).toBe(2);
+    });
+});
+
+describe('SolveProblem([1, 2, 0])', function() {
+    it('should return 3', function() {
+        expect(SolveProblem([1, 2, 0])).toBe(3);
+    });
+});
