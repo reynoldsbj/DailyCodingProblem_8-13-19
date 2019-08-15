32c4f0770628a86bad7d99c62b2d2c230ca714ed
Brian <brianjreynolds@hotmail.com>
2019-08-15 01:22:24 -0400

    Solved problems, added tests, and set up tests for watching.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /**
- * Description: Given an array of integers, find the first missing positibe integer in linear time
+ * Description: Given an array of integers, find the first missing positive integer in linear time
  * and constant space. In other words, find the lowest positive integer that does not exist in the
- * array. The array an contain duplicates and negative numbers as well. 
+ * array. The array can contain duplicates and negative numbers as well. 
  * 
  * Examples:
  *  1: For the input [3, 4, -1, 1], return 2. 
@@ -9,3 +9,37 @@
  * 
  * Note: You can modify the input array in place.
  */
+
+function CreateFalseArray(length) {
+    var arr = [];
+     for (var i=0; i<arr.length; i++) {
+         arr.push(false);
+     }
+
+     return arr;
+}
+
+function FlagValsNotMissing(arr, notMissingFlagArray) {
+    for (var i=0; i<arr.length; i++) {
+         if (arr[i] > 0 && arr[i] < arr.length + 1) {
+             notMissingFlagArray[arr[i]-1] = true;
+         }
+     }
+}
+
+function FindFirstMissingPositiveInt(arr) {
+    var i = 0;
+     while (arr[i] === true) {
+         i++;
+     }
+
+     return i+1;
+}
+
+ function SolveProblem(arr) {
+     //var notMissingFlagArray = new Array(arr.length).fill(false);
+     var notMissingFlagArray = CreateFalseArray(arr.lenght);
+     FlagValsNotMissing(arr, notMissingFlagArray);
+     return FindFirstMissingPositiveInt(notMissingFlagArray);
+ }
+ 
--- test/main.Spec.js
+++ test/main.Spec.js
@@ -1,11 +1,23 @@
-describe('SolveProblem([3, 4, -1])', function() {
+describe('SolveProblem([3, 4, -1, 1])', function() {
     it('should return 2', function() {
-        expect(SolveProblem([3, 4, -1])).toBe(2);
+        expect(SolveProblem([3, 4, -1, 1])).toBe(2);
     });
 });
 
 describe('SolveProblem([1, 2, 0])', function() {
     it('should return 3', function() {
         expect(SolveProblem([1, 2, 0])).toBe(3);
+    });
+});
+
+describe('SolveProblem([1, 1, 1])', function() {
+    it('should return 3', function() {
+        expect(SolveProblem([1, 1, 1])).toBe(2);
+    });
+});
+
+describe('SolveProblem([1, 3, 2])', function() {
+    it('should return 3', function() {
+        expect(SolveProblem([1, 3, 2])).toBe(4);
     });
 });
